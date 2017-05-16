bbecbb27e831193a0f6671a6747668f84fec0176
Cesar Morones <cesar.morones1@gmail.com>
2017-05-16 16:23:58 -0500

    commit antes

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=gestion_iems',
+    'dsn' => 'pgsql:host=10.12.0.171;dbname=gestion_iems',
     'username' => 'kylix',
     'password' => '2020kylix',
     'charset' => 'utf8',
